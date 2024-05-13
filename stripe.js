2e78708ed2cb63afea8f8840e97a859fc2336494
Harry-Kwesi <harrydegraft@hotmail.com>
2024-05-13 09:32:16 +0530

    removed unwanted code

--- stripe.js
+++ stripe.js
@@ -1,28 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const stripe = require("stripe")("");
+const stripe = require("stripe")(
+  "sk_test_51OnBx0Jk50s0cyZJTVLG9CUmuzYtFuVV4C6ILMUNjOsJKwYIhpp7MGUBFEzVG1gq23rcWAHOiQd1AbUz3DM969EF003vljxOuq"
+);
 const port = 8000;
 
-// Route to fetch customer's cards
-app.get("/v1/customers/:id/cards", async (req, res) => {
-  try {
-    const customerId = "cus_PnuOphxSbYxAtB";
-
-    // Retrieve customer's cards using the Stripe API
-    const cards = await stripe.customers.listSources(customerId, {
-      object: "card",
-    });
-
-    res.json({ cards: cards.data });
-  } catch (error) {
-    console.error("Error fetching customer's cards:", error);
-    res.status(500).json({ error: "Failed to fetch customer's cards" });
-  }
-});
-
 app.post("/payment-sheet", async (req, res) => {
-  // Use an existing Customer ID if this is a returning customer.
   const customer = await stripe.customers.create();
   const ephemeralKey = await stripe.ephemeralKeys.create(
     { customer: customer.id },
@@ -32,8 +16,6 @@ app.post("/payment-sheet", async (req, res) => {
     amount: 1099,
     currency: "sgd",
     customer: customer.id,
-    // In the latest version of the API, specifying the `automatic_payment_methods` parameter
-    // is optional because Stripe enables its functionality by default.
     automatic_payment_methods: {
       enabled: true,
     },
@@ -43,7 +25,8 @@ app.post("/payment-sheet", async (req, res) => {
     paymentIntent: paymentIntent.client_secret,
     ephemeralKey: ephemeralKey.secret,
     customer: customer.id,
-    publishableKey: "",
+    publishableKey:
+      "pk_test_51OnBx0Jk50s0cyZJiILj5ALHJiZEpKrASjMYuwcNUU4K5JZ2ty8yhMqkqPXj96h4JKuRqBHHlUjRn23BhKryWHHn00aiCNeH8U",
   });
 });
 
